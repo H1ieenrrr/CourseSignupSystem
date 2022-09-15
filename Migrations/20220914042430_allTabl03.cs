9d65194d663178a7ac68a36627143aab12393453
H1ieenrrr <letanhien.it@gmail.com>
2022-09-15 15:22:47 +0700

    commit : thiết kế database, các chức năng authencation(login, change pass)

---
+++ Migrations/20220914042430_allTabl03.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseSignupSystem.Migrations
+{
+    public partial class allTabl03 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserTeacherCode",
+                table: "User",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserTeacherCode",
+                table: "User");
+        }
+    }
+}
